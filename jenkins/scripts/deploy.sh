ce330dde3c9dc076cebd6373184044abcee79150
1902667-JacksonWong <71168209+1902667-JacksonWong@users.noreply.github.com>
2021-11-22 04:46:11 +0800

    Update deploy.sh

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app2 -v "$PWD":/usr/src php:7.2-apache
+docker container stop
+docker run -d -p 80:80 --name my-apache-php-app3 -v "$PWD":/usr/src php:7.2-apache
 sleep 1
 set +x
 
