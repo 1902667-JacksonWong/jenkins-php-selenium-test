866ac8745b7f00205a5f6af30a0c8bd313dad0a7
1902667-JacksonWong <71168209+1902667-JacksonWong@users.noreply.github.com>
2021-11-22 04:51:14 +0800

    update

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 2000:2000 --name my-apache-php-app4 -v "$PWD":/usr/src php:7.2-apache
+docker run -d -p 4000:4000 --name my-apache-php-app44 -v "$PWD":/usr/src php:7.2-apache
 sleep 1
 set +x
 
